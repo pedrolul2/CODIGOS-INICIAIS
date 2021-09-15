07382680eab523e931900a7f4f94f7e4572a24a7
Pedro Lucas Alves Viana <89168920+pedrolul2@users.noreply.github.com>
2021-09-15 16:11:01 -0300

    Add files via upload

---
+++ While Simples.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+///CONTAR DE 1 A 100////
+int main()
+{
+    int cont=1;
+    //SE ELA FOR MENOR = A 100, SERA EXECUTADAS AS LINHAS DOS SEGUINTES COLCHETES//
+    while(cont<=100){
+        printf("%d\n",cont);
+        ++cont;
+    }
+    
+  return 0;
+  
+}
